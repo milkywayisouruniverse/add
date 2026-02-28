772b29b427329e2eede2a4cadccf075ae5f0321f
milkywayisouruniverse <dazzlingphoenix2022@gmail.com>
2026-02-28 22:30:14 +0530

    5 commit

---
+++ address.py
@@ -0,0 +1,14 @@
+class Delivery:
+    def __init__(self, customer_name, address):
+        self.customer_name = customer_name
+        self.address = address
+
+    def display_delivery_details(self):
+        print("Delivery Details")
+        print("Customer:", self.customer_name)
+        print("Address:", self.address)
+
+
+# Example
+delivery = Delivery("Suman", "Hyderabad")
+delivery.display_delivery_details()
